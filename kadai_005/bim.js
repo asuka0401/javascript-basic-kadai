f50cd22cf3ecd4a93392443c4c79f9fee1be6ff7
asuka0401 <ehimeshakyouchimanebunka@gmail.com>
2025-05-03 20:17:14 +0900

    Add files via upload

---
+++ kadai_005/bim.js
@@ -0,0 +1,13 @@
+//BMI
+let bodyWeiht
+let heigt
+let bmi
+
+//体重［㎏］,身長［m］
+bodyWeiht=68
+heigt=1.70
+bmi=bodyWeiht/heigt/heigt
+
+//BMIの計算方法＝[体重(kg)]÷[身長(m)×身長(m)]
+console.log('あなたのBMI指数は'+bmi);
+
